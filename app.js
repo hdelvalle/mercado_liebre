19254316b0f7e1da2f3411794ef21a056ef95cb8
hdelvalle <dvhernan@gmail.com>
2022-07-10 19:35:42 -0300

    configuracion de puerto para heroku

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const path = require('path');
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
+// const publicPath = path.resolve(__dirname, './public');
+// app.use(express.static(publicPath));
 
-app.listen(3030, () => {
-    console.log("Servidor corriendo en puerto 3030")
-});
+const port = process.env.PORT || 3000;
+app.use(express.static("public"));
+
+app.listen(port, () => console.log(`Server running in port ${port}`));
+// app.listen(3030, () => {
+//     console.log("Servidor corriendo en puerto 3030")
+// });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
